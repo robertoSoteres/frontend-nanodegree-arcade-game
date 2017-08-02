f378e4bc647f38b3ea4d2b2fb7b74534a4fe6fa9
Roberto Soteres <robertosoteres1986@hotmail.com>
2017-08-02 12:59:38 +0200

    Bugs and player
    Creación de los bugs y del player, con sus respectivos movimientos sin que se salgan del espacio de juego.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    
+    //Setting the Enemy initial location (you need to implement)
+    this.x = x;
+    this.y = y;
+    
+    //Setting the Enemy speed (you need to implement)
+    this.speed = 100 + Math.floor(Math.random() * 150);
+    
 };
+
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +25,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 505){
+        this.x = 1;
+    }
+    
+    // Controlamos el choque del bicho con el jugador
+    
+/*    if (this.x === player.prototype.x && this.y === player.prototype.y)
+        {
+            player.prototype.x = 200;
+            player.prototype.y = 400;
+        }*/
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +44,71 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var player = function(x, y) {
+    this.speed = 100;
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;    
+};
 
+
+
+player.prototype.update = function (){
+    if (this.x > 402){
+        this.x = 402;
+    }
+    if (this.x < 0){
+        this.x = 0;
+    }
+    if (this.y > 400){
+        this.y = 400;
+    }
+    if (this.y < 35){
+        this.y = 400;
+        this.x = 200;
+    }
+};
+
+
+player.prototype.render = function (){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+player.prototype.handleInput = function (allowedKey) {
+    switch(allowedKey){
+        case 'up':
+            this.y -= 83;
+            break;
+        case 'down':
+            this.y += 83;
+            break;
+        case 'left':
+            this.x -= 101;
+            break;
+        case 'right':
+            this.x += 101;
+            break;
+    }
+                     
+};
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
+// Place all enemy objects in an array called allEnemies
+var bug1 = new Enemy(100,60);
+var bug2 = new Enemy(0,225);
+var bug3 = new Enemy(300,145);
+var allEnemies = [bug1, bug2, bug3];
+
+
+// Place the player object in a variable called player
+var player = new player(200,400);
 
 
 // This listens for key presses and sends the keys to your
