d2ca5c362d2f680222818e7842db509f8ba23bce
Roberto Soteres <robertosoteres1986@hotmail.com>
2017-08-21 11:13:45 +0200

    more enemies

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var GEM_LIST = ['images/Gem_Blue.png', 'images/Gem_Green.png', 'images/Gem_Orang
 var ENEMIES_LIST = ['images/char-boy.png', 'images/char-cat-girl.png', 'images/char-horn-girl.png', 'images/char-pink-girl.png', 'images/char-princess-girl.png'];
 var Y_POSITION = [60,145,234];
 var TOTAL_GEMS = 3;
-var TOTAL_ENEMIES = 1;
+var TOTAL_ENEMIES = 5;
 var UP_DOWN = 83;
 var LEFT_RIGHT = 101;
 
